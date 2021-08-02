271072f116bdbe9015c82577d00899039c834e41
eric373 <ericryanco@gmail.com>
2021-08-02 14:34:21 -0400

    Created a logging client that uses NLog

---
+++ SampleLoggingClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NLog;
+
+namespace SampleLoggingClient
+{
+    class Program
+    {
+        private static readonly Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int prev = -1;
+            while(true)
+            {
+                logger.Info($"Previous random number was {prev}");
+                int num = random.Next(1, 10);
+                logger.Info($"Random number is {num}");
+                logger.Info(String.Format("Sum of current and previous random numbers is {0}", num + prev));
+                prev = num;
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+    }
+}
