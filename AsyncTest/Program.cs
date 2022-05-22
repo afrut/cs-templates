1a7e768e4d5d1efed6c11cd013219843c6df3566
eric373 <25942990+eric373@users.noreply.github.com>
2022-05-22 18:26:14 -0400

    Run on background threads and wait for all threads to complete

--- AsyncTest/Program.cs
+++ AsyncTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace AsyncTest
 {
@@ -14,21 +15,20 @@ namespace AsyncTest
 
         static async Task Main(string[] args)
         {
-            var tasks = new Task[10];
-            tasks[0] = Task.Run(() => someFunc("1", 1));
-            tasks[1] = Task.Run(() => someFunc("2", 2));
-            tasks[2] = Task.Run(() => someFunc("3", 3));
-            tasks[3] = Task.Run(() => someFunc("4", 4));
+            var tasks = new List<Task>();
+            tasks.Add(Task.Run(() => someFunc("1", 1)));
+            tasks.Add(Task.Run(() => someFunc("2", 2)));
+            tasks.Add(Task.Run(() => someFunc("3", 3)));
+            tasks.Add(Task.Run(() => someFunc("4", 4)));
             Console.WriteLine("Going to do something else");
             for (int i = 0; i < 2.5 * (int)Math.Pow(10, 7); i++) { }
             Console.WriteLine("Finished. Waiting for other tasks");
-            await tasks[0];
+
+            // Wait for all tasks to complete
+            await Task.WhenAll(tasks);
             Console.WriteLine("Task 0 completed");
-            await tasks[1];
             Console.WriteLine("Task 1 completed");
-            await tasks[2];
             Console.WriteLine("Task 2 completed");
-            await tasks[3];
             Console.WriteLine("Task 3 completed");
             Console.ReadLine();
         }
